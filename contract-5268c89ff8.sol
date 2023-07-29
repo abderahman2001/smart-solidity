583523572636314794481f991b414f80673f0c3b
abderahman2001 <58263536+abderahman2001@users.noreply.github.com>
2023-07-29 09:29:39 +0100

    Update contract-5268c89ff8.sol

--- contract-5268c89ff8.sol
+++ contract-5268c89ff8.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts@4.8.3/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.8.3/access/Ownable.sol";
 
 contract CRYPTO is ERC721, Ownable {
-    constructor() ERC721("CRYPTO", "CRP") {}
+    constructor() ERC721("CRYPTOCURRENCY", "CTP") {}
 
     function safeMint(address to, uint256 tokenId) public onlyOwner {
         _safeMint(to, tokenId);
